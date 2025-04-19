2ebf990a51f5458c4d4fd080a64918443ca164e3
aguechfirass100 <aguechfirass100@gmail.com>
2025-04-19 02:04:03 +0100

    Implementing the customer service

---
+++ services/customer/src/main/java/com/agfirass/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,11 @@
+package com.agfirass.ecommerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstname,
+        String lastname,
+        String email,
+        Address address
+) {
+
+}
---
+++ services/customer/src/main/java/com/agfirass/ecommerce/handler/ErrorResponse.java
@@ -0,0 +1,9 @@
+package com.agfirass.ecommerce.handler;
+
+import java.util.Map;
+
+public record ErrorResponse(
+        Map<String, String> errors
+) {
+
+}
